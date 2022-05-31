11633649530e0fe967a0b2a2d32672b4465c5358
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-05-31 13:30:05 +0200

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,11 @@
 let mix = require('laravel-mix')
 const path = require('path')
 
-mix.setPublicPath('dist')
-    .js('resources/js/tool.js', 'js').vue({ version: 3 })
+require('./nova.mix.js');
+
+mix
+    .setPublicPath('dist')
+    .js('resources/js/tool.js', 'js')
+    .vue({ version: 3 })
     .sass('resources/sass/tool.scss', 'css')
-    .alias({
-        'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-    })
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        output: {
-            uniqueName: 'sloveniangooner/locale-anywhere',
-        },
-    })
+    .nova('sloveniangooner/locale-anywhere');
