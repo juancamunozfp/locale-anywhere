d5a0bb32be889e50e7ef0d8b680e549ffc5a0d2f
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-05-31 14:03:56 +0200

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,11 @@
 let mix = require('laravel-mix')
-const path = require('path')
+let path = require('path')
 
 require('./nova.mix.js');
 
 mix
     .setPublicPath('dist')
-    .js('resources/js/tool.js', 'js')
+    .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
     .sass('resources/sass/tool.scss', 'css')
     .nova('sloveniangooner/locale-anywhere');
