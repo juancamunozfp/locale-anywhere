a71331bfd59e66b5d427e3015ce270bfcbc585de
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-05-31 12:24:01 +0200

    nova 4

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,12 +1,12 @@
 Nova.booting((Vue, router, store) => {
-  Nova.inertia('LocaleAnywhere', require('./components/Tool.vue').default)
+  Vue.component('LocaleAnywhere', require('./components/Tool').default)
 
-  Vue.component('locale-anywhere-dropdown', require('./components/Dropdown.vue'));
-  Vue.component('index-locale-anywhere', require('./components/IndexField.vue'));
-  Vue.component('detail-locale-anywhere', require('./components/DetailField.vue'));
-  Vue.component('form-locale-anywhere', require('./components/FormField.vue'));
+  Vue.component('locale-anywhere-dropdown', require('./components/Dropdown').default);
+  Vue.component('index-locale-anywhere', require('./components/IndexField').default);
+  Vue.component('detail-locale-anywhere', require('./components/DetailField').default);
+  Vue.component('form-locale-anywhere', require('./components/FormField').default);
 
   if (window.config !== undefined && window.config.customDetailToolbar) {
-    Vue.component('custom-detail-toolbar', require('./components/CustomDetailToolbar.vue'));
+    Vue.component('custom-detail-toolbar', require('./components/CustomDetailToolbar').default);
   }
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,17 @@
 let mix = require('laravel-mix')
 const path = require('path')
 
-mix.alias({
-    'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-})
-
 mix.setPublicPath('dist')
-    .js('resources/js/tool.js', 'js').vue()
+    .js('resources/js/tool.js', 'js').vue({ version: 3 })
     .sass('resources/sass/tool.scss', 'css')
+    .alias({
+        'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
+    })
+    .webpackConfig({
+        externals: {
+            vue: 'Vue',
+        },
+        output: {
+            uniqueName: 'sloveniangooner/locale-anywhere',
+        },
+    })
